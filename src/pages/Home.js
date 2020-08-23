e96e946c12be6a28246872ec33a16d5171b3f7e9
Santiago Marulanda Molina <santiagomm1997@gmail.com>
2020-08-23 01:49:57 -0500

    Skipping unnecessary re-renders in Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,19 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { withLayout } from '../hoc/withLayout'
 import { PhotosListWithQuery } from '../containers/PhotosListWithQuery'
 
-const Home = (props) => {
+const HomePage = (props) => {
   const { params } = props.match;
   return (
     <PhotosListWithQuery categoryId={params.categoryId} />
   )
 }
+
+const Home = React.memo(HomePage, (prevProps, props) => {
+  const { params: prevParams } = prevProps.match
+  const { params } = props.match
+  return prevParams.categoryId === params.categoryId;
+})
 
 export default withLayout(Home, {
   categoryList: true,
